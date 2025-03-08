a045656b4d91a047e8aeb3ae303aeb82ae00e085
Tausifqureshi <tausifqureshi504@gmail.com>
2025-03-08 14:30:25 +0530

    Filter

--- Script/login.js
+++ Script/login.js
@@ -8,7 +8,7 @@ window.addEventListener("load", function () {
   if (userLogin) {
     window.location.replace("./dashboard.html");
   }
-});
+});       
 
 function login(event) {
   event.preventDefault();
